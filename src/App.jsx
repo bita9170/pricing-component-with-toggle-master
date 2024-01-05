bd435a9ac1c5dacf6d8c4668c5a6809c2f2a277f
bita9170 <bita.s9170@gmail.com>
2024-01-05 16:49:39 +0100

    [FM-4] added main & card container + styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,56 @@ function App() {
   };
 
   return (
+    <>
     <header>
       <h1>Our Pricing</h1>
+
       <div className="toggle-container">
         <p>Annually</p>
+
         <div
           className={`toggle-button ${isOn ? "on" : "off"}`}
           onClick={toggle}
         >
           <div className="slider"></div>
         </div>
+
         <p>Monthly</p>
-        
       </div>
+
     </header>
+
+    <main>
+      <div className="card-container">
+        <h2>Basic</h2> 
+        <h1>$19.99</h1>
+        <p>500 GB Storage</p> 
+        <p>2 Users Allowed</p> 
+        <p>Send up to 3 GB</p> 
+        <button>LEARN MORE</button> 
+      </div>
+
+      <div className="card-container">
+        <h2>Basic</h2> 
+        <h1>$19.99</h1>
+        <p>500 GB Storage</p> 
+        <p>2 Users Allowed</p> 
+        <p>Send up to 3 GB</p> 
+        <button>LEARN MORE</button> 
+      </div>
+
+      <div className="card-container">
+        <h2>Basic</h2> 
+        <h1>$19.99</h1>
+        <p>500 GB Storage</p> 
+        <p>2 Users Allowed</p> 
+        <p>Send up to 3 GB</p> 
+        <button>LEARN MORE</button> 
+      </div>
+
+    </main>
+
+    </>
   );
 }
 
