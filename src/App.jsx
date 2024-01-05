2b75f42ecb278a8c3544aa238008207c7dd81791
bita9170 <bita.s9170@gmail.com>
2024-01-05 14:50:34 +0100

    [FM-4] added toggle button & styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOn, setOn] = useState(false);
+
+  const toggle = () => {
+    setOn(!isOn);
+  };
 
   return (
-    <>
+    <header>
+      <h1>Our Pricing</h1>
       <div>
-      <h1>Hello world</h1>
+        <div
+          className={`toggle-button ${isOn ? "on" : "off"}`}
+          onClick={toggle}
+        >
+          <div className="slider"></div>
+        </div>
+        
       </div>
-      
-    </>
-  )
+    </header>
+  );
 }
 
-export default App
+export default App;
