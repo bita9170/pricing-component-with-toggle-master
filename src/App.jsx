265cfb94a03f16b225a0ae4d3620c6a24c124fa8
bita9170 <bita.s9170@gmail.com>
2024-01-06 03:59:25 +0100

    [FM-4] added ternary operator & edited div content

--- src/App.jsx
+++ src/App.jsx
@@ -9,54 +9,51 @@ function App() {
 
   return (
     <>
-    <header>
-      <h1>Our Pricing</h1>
+      <header>
+        <h1>Our Pricing</h1>
 
-      <div className="toggle-container">
-        <p>Annually</p>
+        <div className="toggle-container">
+          <p>Annually</p>
 
-        <div
-          className={`toggle-button ${isOn ? "on" : "off"}`}
-          onClick={toggle}
-        >
-          <div className="slider"></div>
+          <div
+            className={`toggle-button ${isOn ? "on" : "off"}`}
+            onClick={toggle}
+          >
+            <div className="slider"></div>
+          </div>
+
+          <p>Monthly</p>
+        </div>
+      </header>
+
+      <main>
+        <div className="card-container">
+          <h2>Basic</h2>
+          <h1>{isOn ? "$19.99" : "$199.99"}</h1>
+          <p>500 GB Storage</p>
+          <p>2 Users Allowed</p>
+          <p>Send up to 3 GB</p>
+          <button>LEARN MORE</button>
         </div>
 
-        <p>Monthly</p>
-      </div>
+        <div className="card-container">
+          <h2>Professional</h2>
+          <h1>{isOn ? "$24.99" : "$249.99"}</h1>
+          <p>1 TB Storage</p>
+          <p>5 Users Allowed</p>
+          <p>Send up to 10 GB</p>
+          <button>LEARN MORE</button>
+        </div>
 
-    </header>
-
-    <main>
-      <div className="card-container">
-        <h2>Basic</h2> 
-        <h1>$19.99</h1>
-        <p>500 GB Storage</p> 
-        <p>2 Users Allowed</p> 
-        <p>Send up to 3 GB</p> 
-        <button>LEARN MORE</button> 
-      </div>
-
-      <div className="card-container">
-        <h2>Basic</h2> 
-        <h1>$19.99</h1>
-        <p>500 GB Storage</p> 
-        <p>2 Users Allowed</p> 
-        <p>Send up to 3 GB</p> 
-        <button>LEARN MORE</button> 
-      </div>
-
-      <div className="card-container">
-        <h2>Basic</h2> 
-        <h1>$19.99</h1>
-        <p>500 GB Storage</p> 
-        <p>2 Users Allowed</p> 
-        <p>Send up to 3 GB</p> 
-        <button>LEARN MORE</button> 
-      </div>
-
-    </main>
-
+        <div className="card-container">
+          <h2>Master</h2>
+          <h1>{isOn ? "$39.99" : "$399.99"}</h1>
+          <p>2 TB Storage</p>
+          <p>10 Users Allowed</p>
+          <p>Send up to 20 GB</p>
+          <button>LEARN MORE</button>
+        </div>
+      </main>
     </>
   );
 }
