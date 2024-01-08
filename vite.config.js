96d82e69e1b82d2f637122d914cc9528dceaa820
bita9170 <bita.s9170@gmail.com>
2024-01-08 14:51:59 +0100

    [FM-4] Updated base url

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pricing-component-with-toggle/",
+  base: "/pricing-component-with-toggle-master/",
   plugins: [react()],
-})
+});
